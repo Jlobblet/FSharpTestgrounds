e727ccfddeff568c2d08aad03b81b3787a7450bf
John Blundell <jlobblet-github@gmx.co.uk>
2020-10-02 14:06:28 +0100

    Add framework

---
+++ FSharpTestgrounds/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open FSharpTestGrounds.Reflection
+
+[<EntryPoint>]
+let main argv =
+    let rec inner () =
+        let methods = getTargetMethods ()
+        (chooseMethod methods).Invoke (obj, Array.empty) |> ignore
+        inner ()
+    inner ()
+    0 // return an integer exit code
---
+++ FSharpTestgrounds/Reflection.fs
@@ -0,0 +1,40 @@
+﻿module FSharpTestGrounds.Reflection
+
+open System
+open System.Reflection
+
+type ReflectionTargetAttribute() = inherit Attribute()
+
+let getMethodsWithAttribute<'attr> () =
+    Assembly.GetExecutingAssembly().GetTypes()
+    |> Array.collect (fun t -> t.GetMethods())
+    |> Array.filter (fun m ->
+        (m.CustomAttributes
+         |> Seq.map (fun a -> a.AttributeType)
+         |> Seq.contains typeof<'attr>
+         )
+        )
+
+let getTargetMethods = getMethodsWithAttribute<ReflectionTargetAttribute>
+    
+let chooseMethod (methods: MethodInfo[]) =
+    let methodsIndexed =
+        methods
+        |> Array.mapi (fun i m -> string i, m)
+
+    let map =
+        methodsIndexed
+        |> Map.ofArray
+            
+    methodsIndexed
+    |> Array.map (fun (k, v) -> sprintf "%s: %O" k v.Name)
+    |> String.concat "\n"
+    |> printfn "%s"
+    
+    let rec getChoice () =
+        Console.Write("Enter selection: ")
+        match Console.ReadLine() with
+        | k when map.ContainsKey k -> map.[k]
+        | _ -> getChoice ()
+
+    getChoice ()
