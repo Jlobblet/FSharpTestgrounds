eb6f7557caa7bd6cd504828e3ebc017cc363ff6d
John Blundell <jlobblet-github@gmx.co.uk>
2020-10-04 17:54:08 +0100

    Add pattern matching demonstration

---
+++ FSharpTestgrounds/Examples/Pattern Matching.fs
@@ -0,0 +1,20 @@
+﻿module FSharpTestgrounds.Examples.``Pattern Matching``
+
+type IntOrBool =
+    | Int of int
+    | Bool of bool
+    
+
+
+let ``Type Reveal Party`` v =
+    match v with
+    | Int i -> printfn "It's an integer! %i" i
+    | Bool b -> printfn "It's a bool! %b" b
+
+
+let ``Demonstrate pattern matching to deconstruct a discriminated union`` () =
+    let x = Int 5
+    let y = Bool true
+        
+    ``Type Reveal Party`` x // It's an integer! 5
+    ``Type Reveal Party`` y // It's a bool! true
