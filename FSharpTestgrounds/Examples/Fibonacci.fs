acfd0f8fbc2104fd89b67b580fe2fe18331652ba
John Blundell <jlobblet-github@gmx.co.uk>
2020-10-04 17:51:53 +0100

    Add Fibonacci

---
+++ FSharpTestgrounds/Examples/Fibonacci.fs
@@ -0,0 +1,17 @@
+﻿module FSharpTestgrounds.Examples.Fibonacci
+
+open FSharpTestGrounds.Reflection
+
+let fib n =
+    let rec inner acc1 acc2 n =
+        match n with
+        | 0 -> acc1
+        | 1 -> acc2
+        | _ -> inner acc2 (acc1 + acc2) (n - 1)
+    inner 0 1 n
+    
+[<ReflectionTarget>]
+let ``Print the first 10 Fibonacci numbers`` () =
+    [ 1 .. 10 ]
+    |> List.map fib
+    |> printfn "%A"
