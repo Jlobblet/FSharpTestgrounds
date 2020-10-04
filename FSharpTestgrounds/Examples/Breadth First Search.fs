a4f1c6690e0b161e8ce29549f890906e59ba5a2c
John Blundell <jlobblet-github@gmx.co.uk>
2020-10-04 17:51:24 +0100

    Add breadth first search

---
+++ FSharpTestgrounds/Examples/Breadth First Search.fs
@@ -0,0 +1,12 @@
+﻿module FSharpTestgrounds.Examples.``Breadth First Search``
+
+type Node<'a> = {
+    value: 'a
+    children: Node<'a> list
+}
+
+let rec bfs pred queue =
+    match queue with
+    | [] -> None
+    | x :: _ when pred x -> Some x
+    | x :: xs -> bfs pred (xs @ x.children)
