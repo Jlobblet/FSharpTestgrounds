a80cab55f384533ec11afdb547c1bf2ddedb3ee0
John Blundell <jlobblet-github@gmx.co.uk>
2020-10-04 17:58:50 +0100

    Add reflection target to demo function

--- FSharpTestgrounds/Examples/Pattern Matching.fs
+++ FSharpTestgrounds/Examples/Pattern Matching.fs
@@ -1,17 +1,18 @@
 ﻿module FSharpTestgrounds.Examples.``Pattern Matching``
 
+open FSharpTestGrounds.Reflection
+
 type IntOrBool =
     | Int of int
     | Bool of bool
     
-
-
 let ``Type Reveal Party`` v =
     match v with
     | Int i -> printfn "It's an integer! %i" i
     | Bool b -> printfn "It's a bool! %b" b
 
 
+[<ReflectionTarget>]
 let ``Demonstrate pattern matching to deconstruct a discriminated union`` () =
     let x = Int 5
     let y = Bool true
