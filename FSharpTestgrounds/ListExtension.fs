ba2325bf90bc84b7d7bb1e75016f9be38d435679
John Blundell <jlobblet-github@gmx.co.uk>
2020-10-04 17:51:35 +0100

    Add list extensions

---
+++ FSharpTestgrounds/ListExtension.fs
@@ -0,0 +1,11 @@
+﻿module List
+
+let ipartition predicate list =
+    list
+    |> List.indexed
+    |> List.partition predicate
+    |> fun (t, f) -> (List.map snd t), (List.map snd f)
+    
+let linspace num start stop =
+    let step = (stop - start) / (float num)
+    List.init num (fun i -> (i |> float) * step + start)
