ad9ca5670f13bebe0981b174f4c4cb7edbecdcc5
Jlobblet <jlobblet@gmx.com>
2020-10-21 03:25:27 +0100

    Refactor Reflection.fs

--- FSharpTestgrounds/Reflection.fs
+++ FSharpTestgrounds/Reflection.fs
@@ -3,7 +3,8 @@
 open System
 open System.Reflection
 
-type ReflectionTargetAttribute() = inherit Attribute()
+type ReflectionTargetAttribute() =
+    inherit Attribute()
 
 let getMethodsWithAttribute<'attr> () =
     Assembly.GetExecutingAssembly().GetTypes()
@@ -11,28 +12,25 @@ let getMethodsWithAttribute<'attr> () =
     |> Array.filter (fun m ->
         (m.CustomAttributes
          |> Seq.map (fun a -> a.AttributeType)
-         |> Seq.contains typeof<'attr>
-         )
-        )
+         |> Seq.contains typeof<'attr>))
 
-let getTargetMethods = getMethodsWithAttribute<ReflectionTargetAttribute>
-    
-let chooseMethod (methods: MethodInfo[]) =
+let getTargetMethods =
+    getMethodsWithAttribute<ReflectionTargetAttribute>
+
+let chooseMethod (methods: MethodInfo []) =
     let methodsIndexed =
-        methods
-        |> Array.mapi (fun i m -> string i, m)
+        methods |> Array.mapi (fun i m -> string i, m)
 
-    let map =
-        methodsIndexed
-        |> Map.ofArray
-            
+    let map = methodsIndexed |> Map.ofArray
+
     methodsIndexed
     |> Array.map (fun (k, v) -> sprintf "%s: %O" k v.Name)
     |> String.concat "\n"
     |> printfn "%s"
-    
+
     let rec getChoice () =
         Console.Write("Enter selection: ")
+
         match Console.ReadLine() with
         | k when map.ContainsKey k -> map.[k]
         | _ -> getChoice ()
